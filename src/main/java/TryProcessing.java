3656ed21b07420b3e046b5de57297b966feddfaa
Nitya Arora <nityaarora72@gmail.com>
2021-01-10 21:36:56 +0530

    Code refactored

--- src/main/java/TryProcessing.java
+++ src/main/java/TryProcessing.java
@@ -5,10 +5,16 @@ public class TryProcessing extends PApplet {
     public static final int WIDTH = 640;
     public static final int HEIGHT = 480;
     public static final int DIAMETER = 25;
-    public int xOfFirstBall = 0;
-    public int xOfSecondBall = 0;
-    public int xOfThirdBall = 0;
-    public int xOfForthBall = 0;
+
+    public static final int SPEED_BALL1 = 1;
+    public static final int SPEED_BALL2 = 2;
+    public static final int SPEED_BALL3 = 3;
+    public static final int SPEED_BALL4 = 4;
+
+    private int xPosition_Ball1 = 0;
+    private int xPosition_Ball2 = 0;
+    private int xPosition_Ball3 = 0;
+    private int xPosition_Ball4 = 0;
 
     public static void main(String[] args) {
         PApplet.main("TryProcessing", args);
@@ -27,49 +33,13 @@ public class TryProcessing extends PApplet {
 
     @Override
     public void draw() {
-        drawFirstBall();
-        drawSecondBall();
-        drawThirdBall();
-        drawForthBall();
+        drawCircle(xPosition_Ball1 += SPEED_BALL1, HEIGHT * 0.2f);
+        drawCircle(xPosition_Ball2 += SPEED_BALL2, HEIGHT * 0.4f);
+        drawCircle(xPosition_Ball3 += SPEED_BALL3, HEIGHT * 0.6f);
+        drawCircle(xPosition_Ball4 += SPEED_BALL4, HEIGHT * 0.8f);
     }
 
-    private void drawForthBall() {
-        fillPurpleColor();
-        ellipse(xOfForthBall, HEIGHT * 0.8f, DIAMETER, DIAMETER);
-        xOfForthBall += 4;
-    }
-
-    private void drawThirdBall() {
-        fillYellowColor();
-        ellipse(xOfThirdBall, HEIGHT * 0.6f, DIAMETER, DIAMETER);
-        xOfThirdBall += 3;
-    }
-
-    private void drawSecondBall() {
-        fillRedColor();
-        ellipse(xOfSecondBall, HEIGHT * 0.4f, DIAMETER, DIAMETER);
-        xOfSecondBall += 2;
-    }
-
-    private void drawFirstBall() {
-        fillBlueColor();
-        ellipse(xOfFirstBall, HEIGHT * 0.2f, DIAMETER, DIAMETER);
-        xOfFirstBall++;
-    }
-
-    private void fillRedColor() {
-        fill(255, 0, 0);
-    }
-
-    private void fillYellowColor() {
-        fill(255, 255, 0);
-    }
-
-    private void fillPurpleColor() {
-        fill(128, 0, 128);
-    }
-
-    private void fillBlueColor() {
-        fill(0, 0, 255);
+    private void drawCircle(float xPosition, float yPosition) {
+        ellipse(xPosition, yPosition, DIAMETER, DIAMETER);
     }
 }
